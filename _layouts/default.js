9a31fa7d81c509f9c2a0404a5679427a0ad08695
Giovani Garcia <abelgiovani@gmail.com>
2020-11-13 14:22:16 -0300

    Importado fonts

--- _layouts/default.js
+++ _layouts/default.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@includes/header'
 import Footer from '@includes/footer'
+import 'cirrus-ui'
 
 export default function DefaultLayout(props) {
     return (
@@ -8,7 +9,19 @@ export default function DefaultLayout(props) {
             <Head>
                 <title>{props.title}</title>
                 <meta name='description' content={props.description} />
+
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+    |           <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge;" />
+
+                /* Fonts */
+                <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" />
+
+                /* Fonts Awesome */
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous" />
             </Head>
+            
             <Header />
             {props.children}
             <Footer />
