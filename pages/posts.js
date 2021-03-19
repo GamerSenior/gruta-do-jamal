502cd1544b51fa195cd474e2f7a0248a0ad3b653
Giovani Abel <giovani@prevision.com.br>
2021-03-18 23:09:56 -0300

    Flex funcionando em celulares

--- pages/posts.js
+++ pages/posts.js
@@ -6,36 +6,32 @@ export default function Posts(props) {
     const displayPosts = (posts) => {
         return props.posts.map(function (post, idx) {
             return (
-                <div style={{ width: '350px', justifySelf: 'center' }}>
-                    <Link href={'/posts/' + post.slug} key={idx}>
-                        <div className='card bg-gray-700'>
-                            <div className='card-container'>
-                                <div className='card-image' style={{backgroundImage: `url(${post.image})`}}></div>
-                                <div className='title-container'>
-                                    <p className='title'>{post.title}</p>
-                                </div>
+                <Link href={'/posts/' + post.slug} key={idx}>
+                    <div className='col-3 card bg-gray-700 p-0' >
+                        <div className='card-container'>
+                            <div className='card-image' style={{backgroundImage: `url(${post.image})`}}></div>
+                            <div className='title-container'>
+                                <p className='title'>{post.title}</p>
                             </div>
-                            <div className='content text-light u-text-justify'>
-                                <p>{post.description}</p>
-                            </div>
-
                         </div>
-                    </Link>
-                </div>
+                        <div className='content text-light u-text-justify'>
+                            <p>{post.description}</p>
+                        </div>
+
+                    </div>
+                </Link>
             )
         })
     }
+
     return (
         <DefaultLayout>
-            <div className='px-4 text-light'>
+            <div className='text-light' style={{width: '100%'}}>
                 <div className='u-center'>
                     <h2>Postagens</h2>  
                 </div>
 
-                <div className='grid grid-gap-3 grid-cols-3 u-none-xs'>
-                    {displayPosts(props.posts)}
-                </div>
-                <div className='u-flex-xs u-flex-column u-items-center u-justify-center' style={{width: "100%"}}>
+                <div className='row u-justify-space-evenly'>
                     {displayPosts(props.posts)}
                 </div>
             </div>
